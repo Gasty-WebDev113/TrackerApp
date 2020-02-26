93e7c0b61f2b6a9f127a3f3596e27e4b88ed7671
Gasty-WebDev113 <gastymorales113@gmail.com>
2020-02-26 00:41:51 -0300

    Create the Go Server with Echo Framework

--- backend/api.go
+++ backend/api.go
@@ -1,21 +1,54 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
 )
 
-func RestApi(c *gin.Context){ //Gender Context
-	name := c.Query("name") //Query the data
-	age := c.Query("age")
+func main() {
+	envInit()
+	var port = getEnv("SERVER_PORT", "4000")
 
-	c.JSON(200, gin.H{
-		"Name": name,
-		"Age": age,
+	e := echo.New() //Instance of Echo
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello With to the TrackerApp Server")
 	})
+
+	e.GET("/api", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Work In Progress")
+	})
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
 
-func main() {
-	r := gin.Default()
-	r.GET("/api", RestApi)
-	r.Run(":3000") //Listen in 3000
+func envInit() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file")
+		panic(err.Error())
+	}
+}
+
+func getEnv(key string, defaultValue string) string {
+	if value, exist := os.LookupEnv(key); exist {
+		return value
+	}
+	return defaultValue
+}
+
+func connectToDatabase() {
+	var sqldrivername = getEnv("MYSQL_DRIVER", "")
+	var sqlroot = getEnv("MYSQL_ROOT", "")
+
+	db, err := sql.Open(sqldrivername, sqlroot)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
 }
